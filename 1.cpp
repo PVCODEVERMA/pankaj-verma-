107157beab54e6f470301f5daccf489102803824
pankaj verma <117702000+verma421@users.noreply.github.com>
2023-01-27 01:28:33 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+ using namespace std;
+ int main()
+ {
+   // prints hello world in c++
+   cout <<"Hello world in c++";
+ }
---
+++ 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ using namespace std;
+ int main(){
+   int n1, n2,sum;
+   cout <<"Enter tow number:";
+   cin >> n1 >>n2;
+   //sum of two number is stored in varible sum
+   sum = n1 + n2;
+   //prints sum
+   cout << sum <<endl;
+   return 0;
+ }
